a752ac53b572062b7ecda3642d5db4fd10acaef9
Emanuel Buendia <emanuel_09_27@hotmail.com>
2022-07-25 13:27:04 -0500

    Seccion 11 - obtener los datos de la libreria y guardarlos en una variable para el historial

--- src/app/pages/tab1/tab1.page.ts
+++ src/app/pages/tab1/tab1.page.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 // Plugins
 import { BarcodeScanner } from '@awesome-cordova-plugins/barcode-scanner/ngx';
+import { DataLocalService } from 'src/app/services/data-local.service';
 
 @Component({
   selector: 'app-tab1',
@@ -9,7 +10,7 @@ import { BarcodeScanner } from '@awesome-cordova-plugins/barcode-scanner/ngx';
 })
 export class Tab1Page {
 
-  constructor(private barcodeScanner: BarcodeScanner) { }
+  constructor(private barcodeScanner: BarcodeScanner, private dataLocal: DataLocalService) { }
 
   slidesOpts = {
     allowSlidePrev: false,
@@ -23,8 +24,12 @@ export class Tab1Page {
   scan() {
     this.barcodeScanner.scan().then(barcodeData => {
       console.log('Barcode data', barcodeData);
+      if (!barcodeData.cancelled) {
+        this.dataLocal.guardarRegistro(barcodeData.format, barcodeData.text);
+      }
     }).catch(err => {
       console.log('Error', err);
+      this.dataLocal.guardarRegistro('QRCode', 'https://fernando-herrera.com');
     });
   }
 
